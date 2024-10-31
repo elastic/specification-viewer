748ea933a57056fb401d07ad0de9efdc6b0cf84b
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-10-31 19:18:04 +0000

    use correct case in source file

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,30 +1,17 @@
-import { useState, useEffect, MouseEvent } from 'react';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import RBNavbar from 'react-bootstrap/Navbar';
 import { NavLink, useLocation } from 'react-router-dom';
 
-import { useSchemaContext } from './SchemaContext';
-
-export default function NavBar() {
-  const { version, setVersion, allVersions } = useSchemaContext();
+export default function Navbar() {
   const location = useLocation();
 
   const isActive = (path: string) => {
     return location.pathname.startsWith(path) ? "active": "";
   };
 
-  const changeVersion = (ev: MouseEvent<HTMLElement>) => {
-    ev.preventDefault();
-    const version = ev.currentTarget.textContent;
-    if (version) {
-      setVersion(version);
-    }
-  };
-
   return (
-    <Navbar expand="lg" className="NavBar bg-body-tertiary fixed-top">
-      <Navbar.Brand>
+    <RBNavbar expand="lg" className="Navbar bg-body-tertiary fixed-top">
+      <RBNavbar.Brand>
         <img
           src="/logo-elastic-glyph-color.svg"
           width="30"
@@ -34,22 +21,15 @@ export default function NavBar() {
         />
         &nbsp;
         Elasticsearch Specification Viewer
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      </RBNavbar.Brand>
+      <RBNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <RBNavbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link as={NavLink} to="/" className={isActive("/endpoints")}>Endpoints</Nav.Link>
           <Nav.Link as={NavLink} to="/types" className={isActive("/types")}>Types</Nav.Link>
         </Nav>
-      </Navbar.Collapse>
-      <Navbar.Collapse className="justify-content-end">
-        <NavDropdown title=<span>Elasticsearch version: <b>{version}</b></span>>
-          {allVersions.map(v => (
-            <NavDropdown.Item onClick={changeVersion}>{v}</NavDropdown.Item>
-          ))}
-        </NavDropdown>
-      </Navbar.Collapse>
-    </Navbar>
+      </RBNavbar.Collapse>
+    </RBNavbar>
   );
 }
 
